ca21626b28e655b61ee53e5c8f57fe2a8733f07a
Srujan Gowda KS <kssrujangowda37@gmail.com>
2026-08-18 17:07:46 +0530

    excuted basic sh cmds

---
+++ Session-3(Shell Scripting)/variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+variable="Hello, World!"
+echo $variable
+
+
+name="Srujan Gowda"
+roll_no=10339
+comment="Devopist"
+echo "My name is $name"
+echo "My roll number is $roll_no"
+echo "I am $comment"
